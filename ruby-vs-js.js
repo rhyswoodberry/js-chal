c4c4e1b0202b3a2620a573554e48e8bb5033a0b3
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-14 16:06:37 +1000

    finished challenge 1

--- ruby-vs-js.js
+++ ruby-vs-js.js
@@ -1,19 +1,33 @@
 // Challenge #1
+// If statements
 let user = `Tom`;
 if (user === `Tom`) {
   alert("Hello, Tom.");
 } else {
   alert("Someone who is not Tom.")
 }
-//
+// If statements cont.
 let age = 18
 if (age === 18) {
   alert("Welcome to the casino");
 }
-//
-let user_age = prompt("Please enter your age?");
-if (user_age > 18) {
+// If statements cont.
+let age = prompt("Please enter your age?");
+if (age > 18) {
   alert("Welcome to the casino");
 } else {
   alert("Sorry, you are too young to enter.");
 }
+// Iterate through hash and add user input to array
+let students = []
+students.push(`Sarah`)
+
+let username = prompt("Please enter your name:");
+students.push(username)
+
+for ( let person = 0; person < 3; person++) {
+  let username = prompt("Please enter your name:");
+  students.push(username)
+}
+console.log(students)
+//
