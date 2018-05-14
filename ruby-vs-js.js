a64944a228c6a99aced8a5caf2ca725ccae8a833
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-14 15:45:19 +1000

    create dir and start leaning js

---
+++ ruby-vs-js.js
@@ -0,0 +1,19 @@
+// Challenge #1
+let user = `Tom`;
+if (user === `Tom`) {
+  alert("Hello, Tom.");
+} else {
+  alert("Someone who is not Tom.")
+}
+//
+let age = 18
+if (age === 18) {
+  alert("Welcome to the casino");
+}
+//
+let user_age = prompt("Please enter your age?");
+if (user_age > 18) {
+  alert("Welcome to the casino");
+} else {
+  alert("Sorry, you are too young to enter.");
+}
