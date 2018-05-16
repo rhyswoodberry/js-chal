9b8078312dfb86819861d52841ca247b6d3f10a1
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-16 15:18:31 +1000

    chal

--- bank-sim/scripts.js
+++ bank-sim/scripts.js
@@ -18,3 +18,28 @@ class Account {
   }
 }
 
+
+function randomName() {
+  const firstNames = ["Herb", "Merv", "Berg"]
+  const lastNames = ["Walker", "Wilson", "Wilkins"]
+
+  return randomChoice(firsNames) + " " + randomChoice(lastNames);
+}
+
+function randomChoice(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * ((max + 1) - min)) + min;
+}
+
+// Instances of accounts
+
+const accounts = [];
+
+for(let i = 0; i < 10; i++) {
+  // create new account
+  let newAccount = new Account(randomName(),randomInt(10000000, 99999999), randomInt(0, 100000000000));
+  accounts.push(newAccount);
+}
