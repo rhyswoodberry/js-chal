417b00f3bb97ada2da3342daa6d88a90332b495a
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-16 17:02:07 +1000

    classes

--- classes.js
+++ classes.js
@@ -1,21 +1,25 @@
 class Dog {
-  constructor(name, location) {
+  constructor(name, location, total_distance) {
     this.name = name;
     this.location = location;
+    this.total_distance = total_distance
   }
 
   speak(){
     console.log(`WOOF! My name is ${this.name}`)
   }
 
-  // walk(loaction, distance){
-  //   console.log('I walked to' + ' ' + location + ' ' + distance + 'kms')
-  // }
+  display_walks(place, distance){
+    console.log('I walked to' + ' ' + place + ' ' + 'which was' + ' ' + distance + 'kms')  
+  }
 
+  total_walks(){
+    
+  }
 }
 
-Dog.prototype.walk = function(location, distance){
-  console.log('I walked to' + ' ' + location + ' ' + distance + 'kms')
-}
+// Dog.prototype.walk = function(location, distance){
+//   console.log('I walked to' + ' ' + location + ' ' + distance + 'kms')
+// }
 
 // let dog = new Dog('Che', 'Cuba')
