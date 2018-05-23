dbc77ed11c5833a968e84cd69c50adab8eee1ce4
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-23 15:00:38 +1000

    update ticker function

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -9,7 +9,7 @@ function startStop() {
     // gets the time when start button was pressed
     timeLastStarted = Date.now();
     // start the html ticker at a set interval (100 miliseconds)
-    setInterval(ticker, 100);
+    setInterval(ticker, 10);
 
   } else {
     // change button text
@@ -25,10 +25,11 @@ function startStop() {
 }
 
 function ticker() {
+  let fullElapsed = elapsed;
   // to keep updating the display timer
   if (timeLastStarted !== null) {
-    
+    fullElapsed += (Date.now() - timeLastStarted) / 1000
   }
   // put time in html and fix it to 2 decimal places
-  $("#timer").html(elapsed.toFixed(2));
+  $("#timer").html(fullElapsed.toFixed(2));
 }
