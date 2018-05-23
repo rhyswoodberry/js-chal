4fd2fb6cf16f57125771c9e9f22a820f0b1b7b04
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-23 14:38:42 +1000

    start stop function/buttons

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -2,4 +2,16 @@ let timeLastStarted = null;
 let elapsed = 0;
 
 function startStop() {
+  // checks if timer is not currently running
+  if (timeLastStarted === null) {
+    // change button text
+    $("#start-stop").html('Stop');
+    
+    timeLastStarted = Date.now();
+
+  } else {
+    $("#start-stop").html('Start');
+
+    timeLastStarted = null;
+  }
 }
