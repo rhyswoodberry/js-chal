8d43ac9b8b8444eb39f1b07f39de9b3f427eee04
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-23 14:48:34 +1000

    timer function add

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -6,12 +6,22 @@ function startStop() {
   if (timeLastStarted === null) {
     // change button text
     $("#start-stop").html('Stop');
-    
+    // gets the time when start button was pressed
     timeLastStarted = Date.now();
 
   } else {
+    // change button text
     $("#start-stop").html('Start');
 
+    // takes the end time and subtracts the begining time to get the time elapsed
+    elapsed += (Date.now() - timeLastStarted) / 1000 // to convert to seconds
+
+    // resets timer to be used again later
     timeLastStarted = null;
+
   }
+}
+
+function ticker() {
+  
 }
