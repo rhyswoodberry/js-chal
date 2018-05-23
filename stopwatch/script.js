be72784740e948bd8ec2fcb5c969f1e4a12bc232
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-23 14:55:20 +1000

    update ticker function

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -8,12 +8,14 @@ function startStop() {
     $("#start-stop").html('Stop');
     // gets the time when start button was pressed
     timeLastStarted = Date.now();
+    // start the html ticker at a set interval (100 miliseconds)
+    setInterval(ticker, 100);
 
   } else {
     // change button text
     $("#start-stop").html('Start');
 
-    // takes the end time and subtracts the begining time to get the time elapsed
+    // takes the current time and subtracts the begining time to get the time elapsed
     elapsed += (Date.now() - timeLastStarted) / 1000 // to convert to seconds
 
     // resets timer to be used again later
@@ -23,5 +25,10 @@ function startStop() {
 }
 
 function ticker() {
-  
+  // to keep updating the display timer
+  if (timeLastStarted !== null) {
+    
+  }
+  // put time in html and fix it to 2 decimal places
+  $("#timer").html(elapsed.toFixed(2));
 }
